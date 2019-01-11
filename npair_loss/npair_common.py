83da1db448bb47850a1117c6f10c8f5f6bc7215a
aie2 <atsushi.ietomi@zozo.com>
2019-01-11 19:27:37 +0900

    modify ok

--- npair_loss/npair_common.py
+++ npair_loss/npair_common.py
@@ -4,6 +4,8 @@ import random
 
 import numpy as np
 import tensorflow as tf
+import tensorflow.contrib.slim as slim
+from tensorflow.contrib.slim.nets import inception
 
 
 def random_seed_set( seed=0 ):
@@ -12,6 +14,7 @@ def random_seed_set( seed=0 ):
     tf.set_random_seed(seed)
     return
 
+# for mnist data.
 def get_tensor_image( image_array ):
     IMAGE_WIDTH = 28  # 画像サイズ：幅
     IMAGE_HEIGHT = 28  # 画像サイズ：高さ
@@ -22,6 +25,7 @@ def get_tensor_image( image_array ):
     image = tf.reshape( image, [IMAGE_HEIGHT,IMAGE_WIDTH,IMAGE_CHANNEL])
     return image
 
+# for mnist data.
 def read_function(example):
     """
     以下で生成されたものが引数.
@@ -42,7 +46,7 @@ def read_function(example):
  
     return anc_image, pos_image, label
 
-
+# for mnist data.
 def read_function2(example):
     parsed_features = tf.parse_single_example(
         example,
@@ -116,7 +120,165 @@ def create_network2( x, TARGET_SIZE, space_name="embedding", reuse=False ):
         affine2 = tf.matmul( activate1, W2) + b2
 
         # 出力層
-        y = tf.nn.softmax(affine2)
+        y = affine2
             
     return y
 
+
+def create_network3( x, TARGET_SIZE, space_name="embedding", reuse=False ):
+    # 2 になっているのはたまたま(初期のものがうまく行かなかった経緯がある.)
+    CONV1_FILTER_SIZE = 3  # フィルターサイズ（幅、高さ）
+    CONV1_FEATURES = 32  # 特徴マップ数
+    CONV1_STRIDE = 1  # ストライドの設定
+    MAX_POOL_SIZE1 = 2  # マップサイズ（幅、高さ）
+    POOL1_STRIDE = 2  # ストライドの設定
+    AFFINE1_OUTPUT_SIZE = 100  # 全結合層（１）の出力数
+    channel_num = 3
+    
+    with tf.variable_scope( space_name, reuse=reuse):  # 変数を共有
+        # CONV層
+        conv1_weight = tf.get_variable(
+            "conv1_weight",
+            shape=[CONV1_FILTER_SIZE, CONV1_FILTER_SIZE, channel_num, CONV1_FEATURES],
+            initializer=tf.initializers.truncated_normal( stddev=0.1 ) )
+        conv1_bias = tf.get_variable(
+            "conv1_bias",
+            shape=[CONV1_FEATURES],
+            initializer=tf.constant_initializer( np.zeros(CONV1_FEATURES)))
+        #return conv1_weight        
+        conv1_inner = tf.nn.conv2d(
+            x, conv1_weight,
+            strides=[1, CONV1_STRIDE, CONV1_STRIDE, 1], padding='SAME')
+        conv1 = tf.nn.bias_add( conv1_inner, conv1_bias )
+        conv1_activate = tf.nn.relu(conv1)
+
+        # POOL層
+        pool1 = tf.nn.max_pool(
+            conv1_activate, ksize=[1, MAX_POOL_SIZE1, MAX_POOL_SIZE1, 1],
+            strides=[1, POOL1_STRIDE, POOL1_STRIDE, 1], padding='SAME' )
+        pool1_shape = pool1.get_shape().as_list()
+        pool1_flat_shape = pool1_shape[1] * pool1_shape[2] * pool1_shape[3]
+        #conv1_shape = conv1_activate.get_shape().as_list()
+        #pool1_flat_shape = conv1_shape[1] * conv1_shape[2] * conv1_shape[3]
+        # pool1_flat = tf.reshape( conv1_activate, [-1, pool1_flat_shape] )  # 2次元に変換  
+        pool1_flat = tf.reshape( pool1, [-1, pool1_flat_shape] )  # 2次元に変換        
+
+        #return pool1_flat
+        
+        # 全結合層1
+        W1 = tf.get_variable(
+            "W1",
+            shape=[pool1_flat_shape, AFFINE1_OUTPUT_SIZE],
+            initializer=tf.initializers.truncated_normal() )
+        b1 = tf.get_variable(
+            "b1",
+            shape=[AFFINE1_OUTPUT_SIZE],
+            initializer=tf.constant_initializer( np.zeros(AFFINE1_OUTPUT_SIZE)))
+        affine1 = tf.matmul(pool1_flat, W1) + b1
+        activate1 = tf.sigmoid(affine1)
+
+        # 全結合層2
+        W2 = tf.get_variable(
+            "W2",
+            shape=[AFFINE1_OUTPUT_SIZE, TARGET_SIZE],
+            initializer=tf.initializers.truncated_normal() )
+        b2 = tf.get_variable(
+            "b2",
+            shape=[TARGET_SIZE],
+            initializer=tf.constant_initializer( np.zeros(TARGET_SIZE)))
+        affine2 = tf.matmul( activate1, W2) + b2
+
+        # 出力層
+        y = affine2
+            
+    return y
+
+
+
+
+def create_feature_network( x, feature_size, space_name="embedding", reuse=False ):
+    # 2 になっているのはたまたま(初期のものがうまく行かなかった経緯がある.)
+    CONV1_FILTER_SIZE = 3  # フィルターサイズ（幅、高さ）
+    CONV1_FEATURES = 32  # 特徴マップ数
+    CONV1_STRIDE = 1  # ストライドの設定
+    MAX_POOL_SIZE1 = 2  # マップサイズ（幅、高さ）
+    POOL1_STRIDE = 2  # ストライドの設定
+    AFFINE1_OUTPUT_SIZE = 100  # 全結合層（１）の出力数
+    
+    with tf.variable_scope( space_name, reuse=reuse):  # 変数を共有
+        # CONV層
+        conv1_weight = tf.get_variable(
+            "conv1_weight",
+            shape=[CONV1_FILTER_SIZE, CONV1_FILTER_SIZE, 1, CONV1_FEATURES],
+            initializer=tf.initializers.truncated_normal( stddev=0.1 ) )
+        conv1_bias = tf.get_variable(
+            "conv1_bias",
+            shape=[CONV1_FEATURES],
+            initializer=tf.constant_initializer( np.zeros(CONV1_FEATURES)))
+        conv1_inner = tf.nn.conv2d(
+            x, conv1_weight,
+            strides=[1, CONV1_STRIDE, CONV1_STRIDE, 1], padding='SAME')
+        conv1 = tf.nn.bias_add( conv1_inner, conv1_bias)
+        conv1_activate = tf.nn.relu(conv1)        
+
+        # POOL層
+        pool1 = tf.nn.max_pool(
+            conv1_activate, ksize=[1, MAX_POOL_SIZE1, MAX_POOL_SIZE1, 1],
+            strides=[1, POOL1_STRIDE, POOL1_STRIDE, 1], padding='SAME' )
+        pool1_shape = pool1.get_shape().as_list()
+        pool1_flat_shape = pool1_shape[1] * pool1_shape[2] * pool1_shape[3]
+        pool1_flat = tf.reshape( pool1, [-1, pool1_flat_shape] )  # 2次元に変換
+        
+        # 全結合層1
+        W1 = tf.get_variable(
+            "W1",
+            shape=[pool1_flat_shape, AFFINE1_OUTPUT_SIZE],
+            initializer=tf.initializers.truncated_normal() )
+        b1 = tf.get_variable(
+            "b1",
+            shape=[AFFINE1_OUTPUT_SIZE],
+            initializer=tf.constant_initializer( np.zeros(AFFINE1_OUTPUT_SIZE)))
+        affine1 = tf.matmul(pool1_flat, W1) + b1
+        activate1 = tf.sigmoid(affine1)
+
+        # 全結合層2
+        W2 = tf.get_variable(
+            "W2",
+            shape=[AFFINE1_OUTPUT_SIZE, feature_size],
+            initializer=tf.initializers.truncated_normal() )
+        b2 = tf.get_variable(
+            "b2",
+            shape=[feature_size],
+            initializer=tf.constant_initializer( np.zeros(feature_size)))
+        affine2 = tf.matmul( activate1, W2) + b2
+
+        # 出力層
+        y = affine2
+            
+    return y
+
+
+# slim.arg_scope については以下がわかりやすい.
+# http://tensorflow.classcat.com/2017/04/16/tensorflow-slim/
+# slim のネットワークを使う際のハマりポイント.
+#  http://tatsyblog.sakura.ne.jp/wordpress/applications/machine-learning/1909/
+def create_inception_network( image_tensor,
+                              cnn_output_num=1024, target_num=512,
+                              is_training=True,
+                              space_name="embedding_net", reuse=False ):
+
+    with tf.variable_scope( space_name, reuse=reuse):  # 変数を共有
+        with slim.arg_scope( inception.inception_v3_arg_scope() ):
+            outputs_cnn, endpoints = inception.inception_v3(
+                image_tensor, num_classes=cnn_output_num, is_training=is_training )
+        normalizer_params = {'is_training': is_training}
+        with slim.arg_scope(
+                [slim.layers.fully_connected],
+                activation_fn=tf.nn.relu,
+                normalizer_fn=slim.batch_norm, normalizer_params=normalizer_params ):
+            hidden = slim.layers.flatten( outputs_cnn )
+            outputs = slim.layers.fully_connected( hidden, target_num )
+            
+    return outputs
+        
+        
--- npair_loss/train_tpu2.py
+++ npair_loss/train_tpu2.py
@@ -1,13 +1,16 @@
 # coding: utf-8
 
 import sys
+import os
 
+# from tensorflow.
 import tensorflow as tf
+from tensorflow.contrib.tpu.python.tpu import async_checkpoint
 from tensorflow.contrib.losses import metric_learning
 
-from npair_common import random_seed_set, read_function2, create_network2
 
-from tensorflow.contrib.tpu.python.tpu import async_checkpoint
+# project module
+from npair_common import random_seed_set, create_inception_network, create_network3
 
 #
 # 以下のサイトは TPU 固有のハマりどころをピックアップしているのでよい.
@@ -16,11 +19,14 @@ from tensorflow.contrib.tpu.python.tpu import async_checkpoint
 
 
 
+def get_fpath_list_in_dir( dname ):
+    return [ os.path.join( dname, fname ) for fname in os.listdir(dname) ]
 
 def create_hooks( loss, params ):
     hooks = []
     async_save_hook = async_checkpoint.AsyncCheckpointSaverHook(
-        checkpoint_dir=params['model_dir'], save_steps=100 )
+        checkpoint_dir=params['model_dir'],
+        save_steps=params["save_steps"] )
     hooks.append( async_save_hook )
     # save_hook = tf.train.CheckpointSaverHook(
     #     params["model_dir"], save_steps=params["save_steps"], saver=tf.train.Saver() )
@@ -37,17 +43,18 @@ def create_hooks( loss, params ):
 # tf.estimator.EstimatorSpec を返す関数として定義する必要がある。
 # ここで渡ってくる params というのは Estimator 作成時の引数の params.
 def model_fn_for_npair_loss( features, labels, mode, params ):
-    TARGET_SIZE = 3  # 教師画像の種類数, 0 - 9 までの 10個.    
+    # TARGET_SIZE = 512  #
+    TARGET_SIZE = 128  #     
     learning_rate = 0.01
     loss, train_op = None, None
     predictions = None
     training_hooks = []
 
     if mode != tf.estimator.ModeKeys.PREDICT:
-        anc_xs = features['anc_image']
-        pos_xs = features['pos_image']
-        ys1 = create_network2( anc_xs, TARGET_SIZE )
-        ys2 = create_network2( pos_xs, TARGET_SIZE, reuse=True )
+        anc_xs = features['prod_img']
+        pos_xs = features['snap_img']
+        ys1 = create_network3( anc_xs, TARGET_SIZE )
+        ys2 = create_network3( pos_xs, TARGET_SIZE, reuse=True )
         loss = metric_learning.npairs_loss( labels, ys1, ys2 )
         optimizer = tf.train.AdamOptimizer( learning_rate )
         if params["use_tpu"]:
@@ -68,27 +75,44 @@ def model_fn_for_npair_loss( features, labels, mode, params ):
     return spec
 
 
+
+# for gs://snap_prod_tpu_training/tfrecords_10000 .
+def read_function4( example, image_size=224 ):
+    parsed_features = tf.parse_single_example(
+        example,
+        features={
+            "item_id": tf.FixedLenFeature((), tf.string ),
+            "prod_img": tf.FixedLenFeature((), tf.string ),
+            "snap_img": tf.FixedLenFeature((), tf.string ), }) # データ構造を解析
+    
+    ret_features = {}
+    for img in [ "prod_img", "snap_img" ]:
+        tmp_image = tf.image.decode_jpeg(parsed_features[img], channels=3 )
+        ret_features[img] = tf.image.resize_bicubic([tmp_image], [image_size, image_size])[0]
+    
+    return ret_features, hash(parsed_features["item_id"])
+
 # tf.estimator.Estimator と違って、
 # tf.contrib.tpu.TPUEstimator の input_fn には params を引数とする関数を渡す必要がある.
-def train_input_fn3(params):
+def train_input_fn4(params):
     NUM_THREADS = 4  # スレッド
-    
-    filenames = tf.constant( [ params["input_data_path"] ] )
-
+    fpath_list = get_fpath_list_in_dir( params["data_dir"] ) 
+    filenames = tf.constant( fpath_list )
     dataset = tf.data.TFRecordDataset(filenames)  # ファイル名を遅延評価するパイプを作成.
-    dataset = dataset.map( read_function2, NUM_THREADS) # ファイル名からデータを作成する遅延評価するパイプを作成.
-    dataset = dataset.shuffle(60000)  # MNIST のデータサイズに合わせている.
+    dataset = dataset.map( read_function4, NUM_THREADS) # ファイル名からデータを作成する遅延評価するパイプを作成.
 
     # TPU においては dataset からとりだす tensor の shape を固定する必要がある。
     # 普通の実行では端数のデータのバッチサイズが変わってしまうので、その分は切り落とすように設定する必要がある.
     # params['batch_size'] は estimator の train メソッド呼び出しの際に estimator によって設定される.
     dataset = dataset.repeat().apply(
-        tf.contrib.data.batch_and_drop_remainder(params['batch_size']))    
+        tf.contrib.data.batch_and_drop_remainder(params['batch_size']))
+    
+    # TPU が計算してる間に次のバッチの用意をするためのおまじないらしい.
+    dataset.prefetch(tf.contrib.data.AUTOTUNE)
+    
     return dataset
 
 def get_tpu_run_config(params):
-    num_shards = 8
-    iterations = 50
     tpu_params_for_conenct = {
         "name": "ietomi-demo-tpu",
         "zone": "us-central1-b",
@@ -108,11 +132,11 @@ def get_tpu_run_config(params):
         save_checkpoints_steps=params["save_steps"],
         session_config=tf.ConfigProto(
             allow_soft_placement=True, log_device_placement=True),
-        tpu_config=tf.contrib.tpu.TPUConfig( iterations, num_shards )
-        # tpu_config=tf.contrib.tpu.TPUConfig(
-        #     per_host_input_for_training=tf.contrib.tpu.InputPipelineConfig.PER_HOST_V2),
+        tpu_config=tf.contrib.tpu.TPUConfig(
+            iterations, num_shards,
+            per_host_input_for_training=tf.contrib.tpu.InputPipelineConfig.PER_HOST_V2),
     )
-
+    
     return run_config
 
 
@@ -123,19 +147,26 @@ def get_my_params( use_tpu, is_local ):
         # input_data_path : TFRecordファイル名（学習用）
         file_data_params = {
             "model_dir": "model_dir_for_npair_loss",
-            "input_data_path" : "npair_train.tfrecord" }
+            "input_data_path" : "tfrecords_10000_train-00000-of-00128" }
     else:
         # TPU では VM のローカルのファイルはうまく見れないらしいので gcs のパスを指定する必要がある.
         file_data_params = {
             "model_dir": "gs://ietomi-test/test_model_log",
-            "input_data_path" : "gs://ietomi-test/npair_train.tfrecord" }
+            "input_data_path" : "gs://ietomi-test/tfrecords_10000_train-00000-of-00128" }
 
     my_params = {
+        "data_dir": "data_dir1",
         "model_dir": file_data_params["model_dir"],  # モデルデータの保存場所.
-        "save_steps": 100,  # 何ステップ毎にセーブするか.
-        'log_step_count_steps': 100,
-        "use_tpu" : use_tpu,
-        "max_steps": 1000,
+        "save_steps": 3,  # 何ステップ毎にセーブするか.
+        'log_step_count_steps': 1,  # 何ステップでログを取るか.
+        "use_tpu" : use_tpu,  # bool, False なら TPU 環境でなくとも動くようにしている.
+        "max_steps": 30, # training のステップ数.
+        "num_shards": 8,  # TPU 一台で 8 個の演算ユニットがあるのでデフォルトは 8 にしておく.
+        "tpu_iterations": 100,  # 一回の TPU 演算において何ステップ分を計算するか.
+        # バッチサイズは num_shards=8( 関数 get_my_params を参照) の倍数でないとダメ.(TPUの制限),
+        # また params にわたすと、estimator の中で設定する名前とかぶるのでダメと言われるので
+        # とりあえず外だしで設定する.
+        "my_batch_size": 40,
         "input_data_path": file_data_params["input_data_path"] }
     return my_params
 
@@ -145,15 +176,10 @@ def main():
     else:
         use_tpu = False  # local で実行する場合はここを False にして実行する。
         
-    random_seed_set(1) # seed=1 はたまたまうまくいったので使っている.
+    random_seed_set(1) # seed=1 はたまたまうまくいったので使っている. dataset のshuffle に影響.
 
-    is_local = False
+    is_local = True
     my_params = get_my_params( use_tpu, is_local )
-
-    # バッチサイズは 8 の倍数でないとダメ.(TPUの制限),
-    # また params にわたすと、estimator の中で設定する名前とかぶるのでダメと言われるので
-    # とりあえず外だしで設定する.
-    batch_size = 40
 
     if my_params["use_tpu"]:
         # ここで TPU への接続情報を設定してる.
@@ -161,21 +187,51 @@ def main():
     else:
         # ローカル実行の場合はとりあえずのものを与えれば ok.
         run_config = tf.contrib.tpu.RunConfig()
+
+    # debug code
+    #
+    # my_params["batch_size"] = 7
+    # dataset = train_input_fn4(my_params)
+    # iterator = tf.data.Iterator.from_structure(dataset.output_types, dataset.output_shapes)
+    # iter_init_op = iterator.make_initializer(dataset)  # イテレータを初期化するオペレータ.
+
+    # features, labels = iterator.get_next()  # 遅延評価されるして返される要素.
+    # anc_xs = features['prod_img']
+    # pos_xs = features['snap_img']
+    # TARGET_SIZE = 100
+    # ys1 = create_network3( anc_xs, TARGET_SIZE )
+    
+    # ys2 = create_network3( pos_xs, TARGET_SIZE, reuse=True )
+    # loss = metric_learning.npairs_loss( labels, ys1, ys2 )
+    # optimizer = tf.train.AdamOptimizer( 0.01 )
+    # train_op = optimizer.minimize( loss, global_step=tf.train.get_global_step() )
+    
+    # with tf.Session() as sess:
+    #     init = tf.global_variables_initializer()
+    #     sess.run(init)  # 変数の初期化処理        
+    #     sess.run(iter_init_op)
+    #     #a = sess.run(tmp)
+    #     #print(a)
+    #     sess.run(train_op)
+    #     #a = sess.run(features)
+    #     #b = sess.run(labels)
         
     tpu_estimator = tf.contrib.tpu.TPUEstimator(
         use_tpu=my_params["use_tpu"],
         model_fn=model_fn_for_npair_loss,
         config=run_config,
-        train_batch_size=batch_size,
-        eval_batch_size=batch_size,
+        train_batch_size=my_params["my_batch_size"],
+        eval_batch_size=my_params["my_batch_size"],
         export_to_tpu=False,
         params=my_params
     )
     
-    tpu_estimator.train( input_fn=train_input_fn3, max_steps=my_params["max_steps"] )
+    tpu_estimator.train( input_fn=train_input_fn4,
+                         max_steps=my_params["max_steps"] )
     
-    print( "train ok")
+    #print( "train ok")
     return
+
 
 
 if __name__ == "__main__":
